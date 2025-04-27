74d8cd0787f55cbf23f842c7d8d0ea7c892bd9c1
Mahdi-Rashidiyan <mahdirashidiyan32@gmail.com>
2025-04-27 05:30:51 +0330

    Add requirements.txt and update settings.py for deploy

--- realface/settings.py
+++ realface/settings.py
@@ -16,7 +16,7 @@ FERNET_KEY = secure_config['FERNET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
